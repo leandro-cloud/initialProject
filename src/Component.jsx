6f0312d01ada47d2d7a3379ab410fe66a81cefb1
leandro-cloud <134974518+leandro-cloud@users.noreply.github.com>
2024-05-15 21:01:50 -0500

    Add elements and states to component

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Component() {
+
+  const [text, setText] = useState()
+  const [update, setUpdate] = useState()
+
+  const textOncChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const buttonOnClick = () => {
+    setUpdate(text)
+  }
+  
   return (
-    <div>My Component</div>
+    <div>
+      <input type="text" value={text} onChange={textOncChange}/>
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto Input: {text}</p>
+      <p>Texto Actualizado: {update}</p>
+    </div>
   )
 }
